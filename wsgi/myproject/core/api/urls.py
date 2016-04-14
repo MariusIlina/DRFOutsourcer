ff186a2055d3b468244f21c3c42f20143790fca0
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-15 00:01:48 +0300

    test

--- wsgi/myproject/core/api/urls.py
+++ wsgi/myproject/core/api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('core.api.views',
     url(r'^hola_mundo_rest/(?P<nombre>\w+)$', 'hola_mundo'),
     url(r'^usuarios/$', 'usuarios'),
-    url(r'^usuarios/(?P<id>\d+)$', 'usuarios')
+    url(r'^usuarios/(?P<id>\d+)$', 'usuarios'),
+    url(r'^todos/$', 'to_do'),
 )
