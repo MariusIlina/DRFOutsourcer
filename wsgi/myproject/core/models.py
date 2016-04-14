4e9908723d9e1faefb93675ebd761b55c5b8e629
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-14 11:41:49 +0300

    test

--- wsgi/myproject/core/models.py
+++ wsgi/myproject/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    fecha_creado = models.DateTimeField(auto_now=True)
+    fecha_finalizado = models.DateTimeField()
+    propietario = models.ForeignKey(User)
+    todo = models.TextField()
