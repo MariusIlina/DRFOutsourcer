2b4d6a199b1bca157a1954a352803c5c8951f4e8
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-14 13:09:43 +0300

    test

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     # GETTING-STARTED: define required django version:
     install_requires=[
         #'Django==1.8.4',
+        'rest_framework'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
