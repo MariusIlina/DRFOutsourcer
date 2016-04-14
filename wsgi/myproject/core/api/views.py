c4a58959facff531ac651dcbcf2cb2a49b857b6c
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-15 02:01:54 +0300

    test

--- wsgi/myproject/core/api/views.py
+++ wsgi/myproject/core/api/views.py
@@ -19,7 +19,7 @@ class TodoView(APIView):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            return Response(serializer.object)
+            return Response(serializer.data)
         else:
             return Response(serializer.errors)
 
