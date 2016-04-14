d3090c9f654b135c5c4dd318bc99ee2e505b127f
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-14 15:17:32 +0300

    api

--- wsgi/myproject/core/api/serializers.py
+++ wsgi/myproject/core/api/serializers.py
@@ -1,1 +1,7 @@
-__author__ = 'marius_ilina'
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
--- wsgi/myproject/core/api/views.py
+++ wsgi/myproject/core/api/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from core.api.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 class HolaMundo(APIView):
+
     def get(self, request, nombre, format=None):
         return Response({'mensaje':'Hola ' + nombre + ' en el mundo de Django Rest Framework'})
 
 hola_mundo = HolaMundo.as_view()
+
+class Usuario(APIView):
+    serializer_class = UserSerializer
+
+    def get(self, request, id=None, format=None):
+        if id != None:
+            users = get_object_or_404(User, pk=id)
+            many = False
+        else:
+            users = User.objects.all()
+            many = True
+        response = self.serializer_class(users, many=many)
+        return Response(response.data)
+
+usuarios = Usuario.as_view()
