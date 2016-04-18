33784ccb9e2ab07a12632ecf3906341349511709
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-18 15:51:28 +0300

    test

--- wsgi/myproject/core/api/urls.py
+++ wsgi/myproject/core/api/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.routers import DefaultRouter
-from core.api.views import SizeViewSet
+from core.api.views import SizeViewSet, ItemViewSet
 
 router = DefaultRouter()
 router.register(r'sizes', SizeViewSet)
-urlpatterns = router.urls
-# urlpatterns = patterns('core.api.views',
+router.register(r'items', ItemViewSet)
+
+urlpatterns = patterns('core.api.views',
 #     # url(r'^hola_mundo_rest/(?P<nombre>\w+)$', 'hola_mundo'),
 #     #url(r'^usuarios/$', 'usuarios'),
 #     #url(r'^items/$', 'items'),
-#     #url(r'^', include(router.urls)),
+    url(r'^', include(router.urls)),
 #     #url(r'^usuarios/(?P<id>\d+)$', 'usuarios', name='usuario'),
 #     #url(r'^sizes/$', 'sizes_all'),
 #     #url(r'^sizes/(?P<id>\d+)$', 'sizes_all', name='sizeintro'),
 #     # url(r'^todos/$', 'to_do'),
 #     # url(r'^valuta/$', 'valuta'),
-# )
+)
