1f4132885296fd41e5b2805962027254418e366e
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-18 20:16:02 +0300

    test

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -138,6 +138,8 @@ STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
-    'rest_framework.authentication.SessionAuthentication',
-    'rest_framework.authentication.TokenAuthentication'
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    }
 }
