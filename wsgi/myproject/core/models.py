cd8f75018cf156dc76a3d3bd8977fba6a8ec2ae1
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-19 21:59:37 +0300

    projects

--- wsgi/myproject/core/models.py
+++ wsgi/myproject/core/models.py
@@ -26,6 +26,13 @@ class Country(models.Model):
         return self.country_name
 
 
+class TimeUnit(models.Model):
+    unit_name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.unit_name
+
+
 class Company(models.Model):
     company_name = models.CharField(max_length=200)
     employees_no = models.PositiveSmallIntegerField(default=0)
@@ -41,3 +48,21 @@ class Company(models.Model):
 
     def __unicode__(self):
         return self.company_name
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    by_company = models.ForeignKey(Company, null=True, blank=True)
+    approximate_duration = models.CharField(max_length=200, null=True, blank=True)
+    approximate_duration_time_unit = models.ForeignKey(TimeUnit, null=True, blank=True)
+    description = models.TextField()
+    slug_name = models.CharField(max_length=200, null=True)
+    required_techs = models.TextField(null=True)
+    approximate_hours_per_week = models.IntegerField(default=0)
+    payment_type = models.ForeignKey(PaymentTypes, null=True, blank=True)
+    payment_amount = models.IntegerField(default=0)
+    currency = models.ForeignKey(Currency, null=True)
+
+    def __unicode__(self):
+        return self.project_name
