80b88117dce257d0eaa32f1183b5c61a53210089
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-19 22:55:49 +0300

    projects

--- wsgi/myproject/core/api/urls.py
+++ wsgi/myproject/core/api/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.routers import DefaultRouter
-from core.api.views import CompanyViewSet, CountryViewSet #SizeViewSet, ItemViewSet
+from core.api.views import PaymentTypesViewSet, CurrencyViewSet
+from core.api.views import TimeUnitViewSet, CountryViewSet
+from core.api.views import CompanyViewSet, ProjectViewSet
 
 router = DefaultRouter()
-router.register(r'companies', CompanyViewSet)
+router.register(r'payment-types', PaymentTypesViewSet)
+router.register(r'currencies', CurrencyViewSet)
+router.register(r'time-units', TimeUnitViewSet)
 router.register(r'countries', CountryViewSet)
+router.register(r'companies', CompanyViewSet)
+router.register(r'projects', ProjectViewSet)
 
 urlpatterns = patterns('core.api.views',
     url(r'^', include(router.urls)),
