b249fe32ec1acd81a389b05fc1076fcd02cce663
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-19 22:58:08 +0300

    projects

--- wsgi/myproject/core/admin.py
+++ wsgi/myproject/core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Company, Country
+from .models import Company, Country, PaymentTypes, Currency, TimeUnit, Project
 
 
 # Register your models here.
 
 admin.site.register(Country)
 admin.site.register(Company)
+admin.site.register(PaymentTypes)
+admin.site.register(Currency)
+admin.site.register(TimeUnit)
+admin.site.register(Project)
