9dd6b34ecd73e6884d4824d80e417be224c2fb09
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-20 13:29:19 +0300

    projects

--- wsgi/myproject/core/api/permissions.py
+++ wsgi/myproject/core/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import BasePermission
 from django.contrib.auth.models import User
 
 SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST']
+OWNER_METHODS = ['PUT', 'PATCH', 'DELETE']
 
 class IsCompanyOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -10,7 +11,18 @@ class IsCompanyOwner(BasePermission):
         """
         if request.method in SAFE_METHODS:
             return True
-        elif request.method == 'PUT' or request.method == 'PATCH':
+        elif request.method in OWNER_METHODS:
             if obj.user == request.user or obj.user.is_staff:
                 return True
             return False
+
+
+class IsProjectOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """
+        Returns true if the authenticated user is the user who created the
+        company that posted the project
+        """
+        if request.method in SAFE_METHODS:
+            return True
+        elif request
