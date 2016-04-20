0abfe2a47a0330a0ffe638bf149a7362c671d3fb
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-20 18:28:20 +0300

    projects

--- wsgi/myproject/core/models.py
+++ wsgi/myproject/core/models.py
@@ -95,7 +95,7 @@ class Project(models.Model):
     payment_amount = models.IntegerField(default=0)
     currency = models.ForeignKey(Currency, null=True)
     min_ppl_required = models.IntegerField(null=True, default=0)
-    category = models.ForeignKey(Category, null=True)
+    #category = models.ForeignKey(Category, null=True)
 
     def __unicode__(self):
         return self.project_name
