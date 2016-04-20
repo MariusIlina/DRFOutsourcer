f7838c41338a8a85fb133a3c9224f5f788e7fff1
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-20 13:20:59 +0300

    projects

--- wsgi/myproject/core/api/permissions.py
+++ wsgi/myproject/core/api/permissions.py
@@ -6,11 +6,11 @@ SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST']
 class IsCompanyOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         """
-        Returns true if the user is the owner of the entity
+        Returns true if the user is the owner of the entity or is staff
         """
         if request.method in SAFE_METHODS:
             return True
         elif request.method == 'PUT' or request.method == 'PATCH':
-            if obj.user == request.user or obj.user.is_superuser:
+            if obj.user == request.user or obj.user.is_staff:
                 return True
             return False
