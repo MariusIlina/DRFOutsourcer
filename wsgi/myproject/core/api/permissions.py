152f823ed8013cb507aa227ce22ef32712797068
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-20 15:37:16 +0300

    projects

--- wsgi/myproject/core/api/permissions.py
+++ wsgi/myproject/core/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 from django.contrib.auth.models import User
 from core.models import Company, Project
 
-SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST']
+SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
 OWNER_METHODS = ['PUT', 'PATCH', 'DELETE']
 
 class IsCompanyOwner(BasePermission):
@@ -10,7 +10,7 @@ class IsCompanyOwner(BasePermission):
         """
         Returns true if the user is the owner of the entity or is staff
         """
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS or request.method == 'POST':
             return True
         elif request.method in OWNER_METHODS:
             if obj.user == request.user or obj.user.is_staff:
@@ -21,8 +21,11 @@ class IsCompanyOwner(BasePermission):
 class IsProjectOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         """
-        Returns true if the authenticated user is the user who created the
+        Returns true:
+         - on PUT/PATCH if the authenticated user is the user who created the
         company that posted the project
+         - on POST if the authenticated user is the user who created the company
+         that is trying to create the project
         """
         if request.method in SAFE_METHODS:
             return True
@@ -30,3 +33,17 @@ class IsProjectOwner(BasePermission):
             if obj.by_company.user == request.user:
                 return True
             return False
+        elif request.method == 'POST':
+            company = Company.objects.get(id=request.id)
+            if company.user == request.user:
+                return True
+            return False
+
+
+class BidderIsCompanyOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """
+        Returns true if the user who wants to bid on a project is the owner
+        of the company on which behalf he does the bidding
+        """
+        return True
