1bb1d9bd7ae926815fdd7df95daeeadbf9d61651
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-04-20 13:09:58 +0300

    projects

--- wsgi/myproject/core/api/permissions.py
+++ wsgi/myproject/core/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from django.contrib.auth.models import User
 
 SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST']
 
@@ -10,6 +11,6 @@ class IsCompanyOwner(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         elif request.method == 'PUT' or request.method == 'PATCH':
-            if obj.user == request.user:
+            if obj.user == request.user or obj.user.is_staff():
                 return True
             return False
