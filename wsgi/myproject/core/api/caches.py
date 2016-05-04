592b4b0094672a4d1c43b012294e73971cf31958
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-05-04 18:07:47 +0300

    redis

--- wsgi/myproject/core/api/caches.py
+++ wsgi/myproject/core/api/caches.py
@@ -14,7 +14,7 @@ class ProjectCache(BaseCache):
             ('id', obj.id),
             ('project_name', obj.project_name),
             self.field_to_json('DateTime', 'pub_date', obj.pub_date),
-            self.field_to_json('by_company', 'by_company', model=Company, pks=obj._company_pks),
+            ('by_company', [obj.by_company]),
             ('approximate_duration', obj.approximate_duration),
             self.field_to_json(
                 'PK',
@@ -46,8 +46,7 @@ class ProjectCache(BaseCache):
 
     def project_default_add_related_pks(self, obj):
         """Add related primary keys to a Project instance."""
-        if not hasattr(obj, '_company_pks'):
-            obj._company_pks = list(obj.by_company.values_list('id', flat=True))
+        return []
 
     def project_default_invalidator(self, obj):
         """Invalidate cached items when the Project changes."""
