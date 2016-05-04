3122a86b165de324fb796a0e4c1ac965f59e6c91
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-05-04 18:21:01 +0300

    redis

--- wsgi/myproject/core/api/caches.py
+++ wsgi/myproject/core/api/caches.py
@@ -14,7 +14,7 @@ class ProjectCache(BaseCache):
             ('id', obj.id),
             ('project_name', obj.project_name),
             self.field_to_json('DateTime', 'pub_date', obj.pub_date),
-            ('by_company', [obj.by_company]),
+            self.field_to_json('PK', 'by_company', model=Company, pk=obj.by_company.id),
             ('approximate_duration', obj.approximate_duration),
             self.field_to_json(
                 'PK',
