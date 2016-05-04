7f902505cdb461a3cc1d0de2edf8a8f4475655a9
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-05-04 16:27:07 +0300

    redis

--- wsgi/myproject/core/api/caches.py
+++ wsgi/myproject/core/api/caches.py
@@ -10,29 +10,29 @@ class ProjectCache(BaseCache):
         """Convert a Project to a cached instance representation."""
         if not obj:
             return None
-        #self.project_default_add_related_pks(obj)
+        self.project_default_add_related_pks(obj)
         return dict((
             ('id', obj.id),
             ('project_name', obj.project_name),
             self.field_to_json('DateTime', 'pub_date', obj.pub_date),
-            self.field_to_json('by_company', 'by_company', model=Company, pks=obj._company_pks),
+            self.field_to_json('by_company', 'by_company', model=Company, pk=obj.by_company.id),
             ('approximate_duration', obj.approximate_duration),
             self.field_to_json(
                 'approximate_duration_time_unit',
                 'approximate_duration_time_unit',
                 model=TimeUnit,
-                pks=obj._time_unit_pks
+                pk=obj.approximate_duration_time_unit.id
             ),
             ('description', obj.description),
             ('work_description', obj.work_description),
             ('slug_name', obj.slug_name),
             ('required_techs', obj.required_techs),
             ('approximate_hours_per_week', obj.approximate_hours_per_week),
-            self.field_to_json('payment_type', 'payment_type', model=PaymentTypes, pks=obj._payment_type_pks),
+            self.field_to_json('payment_type', 'payment_type', model=PaymentTypes, pk=obj.payment_type.id),
             ('payment_amount', obj.payment_amount),
-            self.field_to_json('currency', 'currency', model=Currency, pks=obj._currency_pks),
+            self.field_to_json('currency', 'currency', model=Currency, pk=obj.currency.id),
             ('min_ppl_required', obj.min_ppl_required),
-            self.field_to_json('category', 'category', model=Category, pks=obj._category_pks)
+            self.field_to_json('category', 'category', model=Category, pks=obj.category.id)
         ))
 
     def project_default_loader(self, pk):
@@ -46,17 +46,7 @@ class ProjectCache(BaseCache):
             return obj
 
     def project_default_add_related_pks(self, obj):
-        """Add related primary keys to a Project instance."""
-        if not hasattr(obj, '_company_pks'):
-            obj._company_pks = list(obj.by_company.values_list('id', flat=True))
-        if not hasattr(obj, '_time_unit_pks'):
-            obj._time_unit_pks = list(obj.approximate_duration_time_unit.values_list('id', flat=True))
-        if not hasattr(obj, '_payment_type_pks'):
-            obj._payment_type_pks = list(obj.payment_type.values_list('id', flat=True))
-        if not hasattr(obj, '_currency_pks'):
-            obj._currency_pks = list(obj.currency.values_list('id', flat=True))
-        if not hasattr(obj, '_category_pks'):
-            obj._category_pks = list(obj.category.values_list('id', flat=True))
+        return []
 
     def project_default_invalidator(self, obj):
         """Invalidate cached items when the Project changes."""
