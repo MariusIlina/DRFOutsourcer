d736e106f103bb06f370a1d9b217be599435b773
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-05-06 21:31:49 +0300

    redis

--- wsgi/myproject/core/models.py
+++ wsgi/myproject/core/models.py
@@ -102,7 +102,8 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         try:
             r = settings.REDIS_INIT
-            r.delete(':1:drfc_default_Project_' + self.id)
+            rediskey = ':1:drfc_default_Project_' + self.id
+            r.delete(rediskey)
         except Exception:
             pass
         super(Project, self).save(*args, **kwargs)
