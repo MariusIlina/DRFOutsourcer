647606953cf3f7196e0c304b083b57fccf19ff75
ParamountVenturesLTD <mariusilina2@gmail.com>
2016-05-23 10:24:50 +0300

    pagination

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.DjangoFilterBackend',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 3
 }
 
 import redis
